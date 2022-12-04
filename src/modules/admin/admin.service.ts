0f6790b72980368fd975fae78e524ce69f8e433d
Emily81926 <emily81926@gmail.com>
2022-12-04 11:38:32 +0800

    feat: revise admin feature to populate user data

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose'
 
 import { Model } from 'mongoose'
 
-import { Tweet, TweetDocument } from '../mongodb/schemas'
+import { Tweet, TweetDocument} from '../mongodb/schemas'
 
 @Injectable()
 export class AdminService {
@@ -11,7 +11,7 @@ export class AdminService {
     @InjectModel(Tweet.name) private readonly tweetModel: Model<TweetDocument>
   ) {}
 
-  getTweets() {
-    return this.tweetModel.find()
+  async getTweets() {
+    return await this.tweetModel.find({}).populate('user')
   }
 }

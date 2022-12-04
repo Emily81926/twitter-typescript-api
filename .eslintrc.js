691a5719572c1c9d254d723142ec5c6258c13717
Kerwin <tkoleo84119@gmail.com>
2022-12-04 12:54:47 +0800

    chore: change eslint config setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,9 +18,10 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    semi: ["error", "never"]
+    "semi": ["error", "never"],
+    "quotes": ["error", "single"]
   },
 };
